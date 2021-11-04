cb057d144a4b1355651c4304f491678f4a7293fb
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-11-04 20:23:45 -0300

    promesas - crear mi primera promesa

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# promesas-en-javascript" 
+
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("hola mundo");
+//console.log("hola mundo");
+
+/**Ejemplo de peticion asíncrona */
+function imAsync(){
+  return "Hola promesa";
+}
+
+let message = setTimeout(imAsync, 2000);
+
+console.log(message);
+
+//CREAR MI PRIMERA PROMESA
+
+//executor: function que recibe la promesa
+let promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve("hola promesa");
+    reject(new Error("Hubo un error")); 
+  }, 2000)
+});
+
+promise.then(console.log).catch(console.log);
+
