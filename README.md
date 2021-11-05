d67e54d69ab16c53d64c58d19571baf463d5acd6
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-11-04 22:08:45 -0300

    promesas - responder a una promesa

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# promesas-en-javascript" 
 
-
+Executor: callback de una promesa
+Estados: pending => pendiente, fullfield => completado, rejected => rechazado
--- js/main.js
+++ js/main.js
@@ -14,10 +14,19 @@ console.log(message);
 //executor: function que recibe la promesa
 let promise = new Promise(function(resolve, reject){
   setTimeout(function(){
-    resolve("hola promesa");
-    reject(new Error("Hubo un error")); 
+    if(confirm("¿Esta promesa se cumplió?"))
+      return resolve("hola promesa");
+    return reject(new Error("Hubo un error")); 
   }, 2000)
 });
 
-promise.then(console.log).catch(console.log);
+//promise.then(console.log).catch(console.log);
 
+//RESPONDER A UNA PROMESA
+
+promise.then(function(resultado){
+  console.log(resultado)
+}).catch(function(error){
+  console.log("Algo salió mal");
+  console.log(error);
+})
